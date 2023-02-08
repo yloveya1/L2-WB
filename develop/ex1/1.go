e8fb088b1165a9f4dbe21bdb3ce4208c5313bb35
toddredw <ylovyagina00@gmail.com>
2023-02-08 18:05:37 +0300

    ex1

---
+++ develop/ex1/1.go
@@ -0,0 +1,28 @@
+/*Базовая задача
+
+Создать программу печатающую точное время с использованием NTP -библиотеки. Инициализировать как go module.
+Использовать библиотеку github.com/beevik/ntp. Написать программу печатающую текущее время / точное время с
+использованием этой библиотеки.
+
+Требования:
+Программа должна быть оформлена как go module
+Программа должна корректно обрабатывать ошибки библиотеки: выводить их в STDERR и возвращать ненулевой код выхода в OS
+*/
+
+package ex1
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
+func ex1() (time.Time, error) {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return time, err
+	}
+	return time, nil
+}
---
+++ develop/ex1/go.mod
@@ -0,0 +1,9 @@
+module github.com/yloveya1/ex1
+
+go 1.19
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
